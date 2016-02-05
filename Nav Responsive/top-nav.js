2eee3b9f4ddec59c69c545c3ef1fafe0506b9b67
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-05 01:14:50 -0800

    Event delegation

--- Nav Responsive/top-nav.js
+++ Nav Responsive/top-nav.js
@@ -1,6 +1,36 @@
-var hamburgerIcon = document.getElementsByClassName('icon')[0];
+// Vanilla JS Version
+var domReady = function (callback) {
+  document.readyState === "interactive" ||
+  document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
+};
 
-hamburgerIcon.addEventListener('click', function (e) {
-  e.preventDefault();
-  document.getElementsByClassName('topnav')[0].classList.toggle('responsive');
+domReady(function () {
+  document.addEventListener('click', delegate(criteria, handler));
+
+  function delegate(criteria, handler) {
+    return function (e) {
+      var el = e.target;
+      debugger;
+
+      do {
+        if (!criteria(el)) {
+          continue;
+        }
+
+        e.delegateTarget = el;
+        handler.call(this, e);
+        return false;
+      } while (el = el.parentNode);
+    }
+  }
+
+  function criteria(el) {
+    return el instanceof HTMLElement && el.matches('.icon');
+  }
+
+  function handler(e) {
+    e.preventDefault();
+
+    document.getElementsByClassName('topnav')[0].classList.toggle('responsive');
+  }
 });
